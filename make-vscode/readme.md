f801b1fa51b702a11f650b0c836c8ea8c098d9c1
Adam Gordon Bell <adam@corecursive.com>
2023-01-10 10:25:14 -0500

    adding vs code make file

---
+++ make-vscode/readme.md
@@ -0,0 +1,1 @@
+* install extension "Makefile Tools"
---
+++ make-vscode/src/main/coinflipper.cpp
@@ -0,0 +1,33 @@
+#include <cstdio>
+#include <cstdlib>
+#include <stdlib.h>
+#include <assert.h>
+#include <time.h>  
+
+int flip_coins(int iters) {
+  srand (time(NULL));
+  int tails = 0;
+  int heads = 0;
+
+  for(int i=0;i < iters;i++){
+      int coin = rand() % 2;
+      if(coin == 1) {
+          printf("heads\n");
+          heads++;
+      } else {
+          printf("tails\n");
+          tails++;
+      }
+  }
+  printf("%d Heads, %d Tails\n",heads, tails);
+  return abs(tails-heads);
+}
+
+int main(int argc,char* argv[]) { 
+    assert (argc == 2);
+    int iters = atoi(argv[1]);
+    int diff = flip_coins(iters);
+    if(100 < iters) {
+        printf("With enough trials Heads should be equal to Tails\n");
+    }
+}
